aa082cc3bb0ba7da83d1cb82ceb8232e440042a2
Elmax19 <maksim.kohnovich@mail.ru>
2021-11-19 20:11:21 +0300

    initial commit

---
+++ src/main/java/com/itechart/students_lab/waybill_suppliers/WaybillSuppliersApplication.java
@@ -0,0 +1,13 @@
+package com.itechart.students_lab.waybill_suppliers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaybillSuppliersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaybillSuppliersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itechart/students_lab/waybill_suppliers/WaybillSuppliersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itechart.students_lab.waybill_suppliers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaybillSuppliersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
