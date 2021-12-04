7dbf33946bf6cbaf64d85dca7b4fcd355d496587
Elmax19 <maksim.kohnovich@mail.ru>
2021-12-05 00:57:22 +0300

    configuring slf4j

--- src/main/java/com/itechart/students_lab/waybill_suppliers/exception/handler/RestResponseExceptionHandler.java
+++ src/main/java/com/itechart/students_lab/waybill_suppliers/exception/handler/RestResponseExceptionHandler.java
@@ -2,6 +2,8 @@ package com.itechart.students_lab.waybill_suppliers.exception.handler;
 
 import com.itechart.students_lab.waybill_suppliers.exception.BadRequestException;
 import com.itechart.students_lab.waybill_suppliers.exception.NotFoundException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,23 +13,29 @@ import java.sql.SQLIntegrityConstraintViolationException;
 
 @ControllerAdvice
 public class RestResponseExceptionHandler {
+    private static final Logger LOGGER = LogManager.getLogger(RestResponseExceptionHandler.class);
+
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<String> handleNotFound(NotFoundException e) {
+        LOGGER.error(e.getLocalizedMessage());
         return new ResponseEntity<>(e.getLocalizedMessage(), e.getStatusCode());
     }
 
     @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
     public ResponseEntity<String> handleConstraintViolationException(SQLIntegrityConstraintViolationException e){
+        LOGGER.error(e.getLocalizedMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Entered value is not unique");
     }
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<String> handleBadRequestException(BadRequestException e){
+        LOGGER.error(e.getLocalizedMessage());
         return new ResponseEntity<>(e.getLocalizedMessage(), e.getStatusCode());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
+        LOGGER.error(e.getLocalizedMessage());
         return new ResponseEntity<>(e.getLocalizedMessage(), HttpStatus.BAD_REQUEST);
     }
 }
