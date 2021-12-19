7b36d6d9ac52813e0b9e11f9a46c213ec001561a
Elmax19 <maksim.kohnovich@mail.ru>
2021-12-19 23:39:10 +0300

    changing item: adding customerId field

--- src/main/java/com/itechart/students_lab/waybill_suppliers/repository/ItemRepo.java
+++ src/main/java/com/itechart/students_lab/waybill_suppliers/repository/ItemRepo.java
@@ -1,10 +1,21 @@
 package com.itechart.students_lab.waybill_suppliers.repository;
 
 import com.itechart.students_lab.waybill_suppliers.entity.Item;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @Repository
 public interface ItemRepo extends JpaRepository<Item, Long> {
+    List<Item> findAllByCustomerId(Long id, Pageable pageable);
+
+    Item findByCustomerIdAndId(Long customerId, Long id);
+
+    @Transactional
+    void deleteByCustomerIdAndId(Long customerId, Long id);
 }
