20392d662a0e46e10618acf8ba61aef52fa3d98e
Ilya Chernyavsky <chern200213@gmail.com>
2022-01-08 22:02:35 +0300

    add getTotal method, fix EmployeeDto id

--- src/main/java/com/itechart/students_lab/waybill_suppliers/entity/dto/EmployeeDto.java
+++ src/main/java/com/itechart/students_lab/waybill_suppliers/entity/dto/EmployeeDto.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 @Data
 public class EmployeeDto {
-    private Integer id;
+    private Long id;
     private UserRole role;
     private ContactInformationRecordDto contactInformation;
 }
