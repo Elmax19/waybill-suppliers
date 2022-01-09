e276d129ebecfde0042898c293c1265cecc6bc35
Ilya Chernyavsky <chern200213@gmail.com>
2022-01-09 16:33:57 +0300

    add state taxes map

---
+++ src/main/java/com/itechart/students_lab/waybill_suppliers/controller/ItemCategoryController.java
@@ -0,0 +1,19 @@
+package com.itechart.students_lab.waybill_suppliers.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = {"http:/localhost:3000"})
+@RestController
+@RequiredArgsConstructor
+public class ItemCategoryController {
+
+
+    // get all ItemCategories
+    // get items amount of such category
+    // get all ItemCategories have at least one item
+    //edit ItemCategory
+
+
+}
